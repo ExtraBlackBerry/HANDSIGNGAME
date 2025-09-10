3cfbbb378c53da1af0b9cc7ba88936097f287b3f
Foster <fosterjameskiatarae@gmail.com>
2025-09-10 19:24:35 +1200

    Enhance ScreenManager to include player name in MainMenu initialization and improve button event handling

--- src/classes/screen_manager.py
+++ src/classes/screen_manager.py
@@ -5,7 +5,8 @@ class ScreenManager:
     def __init__(self):
         pygame.init()
         self._screen = pygame.display.set_mode((1280, 720))
-        self._current_screen = MainMenu(self._screen) # Start with main menu
+        self._player_name = "player"
+        self._current_screen = MainMenu(self._screen, self._player_name) # Start with main menu
         self._running = True
         
     def run(self):
@@ -15,11 +16,17 @@ class ScreenManager:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: # Quit button
                     self._running = False
-                self._current_screen.handle_event(event) # Let screen handle event
+                result = self._current_screen.handle_event(event) # Let screen handle event
+                if result == "host":
+                    print("Switch to Host Screen") # TODO: Replace with actual HostScreen
+                if result == "join":
+                    print("Switch to Join Screen")
+                if result == "exit":
+                    self._running = False
         
-        # Display update
-        self._current_screen.show()
-        pygame.display.flip()
+            # Display update
+            self._current_screen.show()
+            pygame.display.flip()
         
 # Test code
 if __name__ == "__main__":
