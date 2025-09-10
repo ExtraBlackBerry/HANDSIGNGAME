d09b619d30f5058898e7897bd49028d9cc180ab9
Foster <fosterjameskiatarae@gmail.com>
2025-09-10 21:24:37 +1200

    Refactor ScreenManager to initialize with LoginScreen and split event handling for each screen

--- src/classes/screen_manager.py
+++ src/classes/screen_manager.py
@@ -1,12 +1,13 @@
-from classes.gui.main_menu_screen import MainMenu
+from gui.main_menu_screen import MainMenu
+from gui.login_screen import LoginScreen
 import pygame
 
 class ScreenManager:
     def __init__(self):
         pygame.init()
         self._screen = pygame.display.set_mode((1280, 720))
-        self._player_name = "player"
-        self._current_screen = MainMenu(self._screen, self._player_name) # Start with main menu
+        self._player_name = "none"
+        self._current_screen = LoginScreen(self._screen)
         self._running = True
         
     def run(self):
@@ -16,15 +17,28 @@ class ScreenManager:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: # Quit button
                     self._running = False
-                result = self._current_screen.handle_event(event) # Let screen handle event
-                if result == "host":
-                    print("Switch to Host Screen") # TODO: Replace with actual HostScreen
-                if result == "join":
-                    print("Switch to Join Screen")
-                if result == "exit":
-                    self._running = False
+                
+                # Login Screen event handling
+                if self._current_screen._screen_name == "LoginScreen":
+                    # Handle name input
+                    name_result = self._current_screen.handle_event(event)
+                    if name_result is not None and name_result.strip() != "":
+                        self._player_name = name_result
+                        self._current_screen = MainMenu(self._screen, self._player_name)
+                        continue
+                
+                # Main menu event handling
+                if self._current_screen._screen_name == "MainMenu":
+                    # Handle buttons
+                    result = self._current_screen.handle_event(event)
+                    if result == "host":
+                        print("Switch to Host Screen") # TODO: Replace with actual HostScreen
+                    if result == "join":
+                        print("Switch to Join Screen")
+                    if result == "exit":
+                        self._running = False
         
-            # Display update
+            # Display
             self._current_screen.show()
             pygame.display.flip()
         
